a522a8eb549a732a4ab94d2850169875aa5d2eba
Fedechkin Aleksei <alex@centralhardware.me>
2024-10-26 13:02:31 +0200

    fix formatting

--- src/main/kotlin/EnvironmentVariableUserAccessChecker.kt
+++ src/main/kotlin/EnvironmentVariableUserAccessChecker.kt
@@ -3,5 +3,6 @@ package me.centralhardware.telegram
 class EnvironmentVariableUserAccessChecker : UserAccessChecker {
     private val allowedUsers =
         (System.getenv("ALLOWED_USERS") ?: "").let { it.split(",").map(String::toLong) }
+
     override fun checkAccess(userId: Long?) = userId?.let { allowedUsers.contains(userId) } ?: true
 }
